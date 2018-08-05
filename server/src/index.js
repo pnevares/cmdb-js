2381d3b259e2be5ef8f677cc600dc335916125fe
Pablo Nevares <pablo@nevares.com>
2018-08-05 05:16:57 -0700

    Directory for REST routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const restV1Routes = require('./rest-v1-routes');
 
 app.get('/', (req, res) => res.send('Hello world from cmdb-js server.'));
 
@@ -8,5 +9,7 @@ app.get('/api', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.json({ message: 'server is alive' });
 });
+
+app.use('/rest/v1', restV1Routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
