543ee87d98e143f8228e3f56f5b08889cbbc551a
Pablo Nevares <pablo@nevares.com>
2018-08-21 21:56:43 -0700

    Fetching from api in client or server-side rendering mode

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,1 +1,15 @@
-export default () => <div>Welcome to next.js!!</div>
+import fetch from 'isomorphic-unfetch';
+import { apiUrl } from '../src/util';
+
+const App = ({ data }) =>
+  <div>
+    Api response: {data}
+  </div>
+
+App.getInitialProps = async () => {
+  const res = await fetch(`${apiUrl}/api`);
+  const json = await res.json();
+  return { data: json.message };
+}
+
+export default App;
